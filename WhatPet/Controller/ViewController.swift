4b4223c4d8c9ec7f273260b0e036339f1767b136
Eliu Diaz <eedb_abcd_1234567890@ymail.com>
2020-11-01 16:54:02 -0600

    Initial commit

---
+++ WhatPet/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatPet
+//
+//  Created by Eliu Efraín Díaz Bravo on 01/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
